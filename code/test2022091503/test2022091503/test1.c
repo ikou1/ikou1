eb73106777fa964172b647fb36135cb205ea1ac2
ikou <1430435375@qq.com>
2022-09-16 08:49:42 +0800

    max min

--- code/test2022091503/test2022091503/test1.c
+++ code/test2022091503/test2022091503/test1.c
@@ -1,2 +1,14 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
-111
+#include<stdio.h>
+int main()
+{
+	int a, b, c;
+	scanf("%d%%d%d", &a, &b, &c);
+	int max = a >= b ? a : b;
+	max = max >= c ? max : c;
+	int min = a <= b ? a : b;
+	int min = min <= c ? min : c;
+
+
+	return 0;
+}
