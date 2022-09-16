82bf555107e77847be33efd159b90231c5241bf7
ikou <1430435375@qq.com>
2022-09-16 09:57:19 +0800

    素数判断器

--- code/test2022091602/test2022091602/test1.c
+++ code/test2022091602/test2022091602/test1.c
@@ -1,1 +1,31 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void prime(int k)   //ËØÊýÅÐ¶Ï
+{
+	int  i;
+	int count = 0;
+	for (i = (k-1); i >= 1; i--)
+	{
+		if ((k % i) == 0)
+		{
+			break;
+		}
+		else
+		{
+			count++;
+		}
+	}
+	if (count == k - 2)
+	{
+		printf("%dÊÇËØÊý\n", k);
+	}
+}
+int main()
+{
+	int i;
+	for (i = 100; i <= 200; i++)
+	{
+		prime(i);
+	}
+	return 0;
+}
