3dd6b1d2d978dde67f274136876a0ec2a36a55e5
ikou <1430435375@qq.com>
2022-09-16 09:27:09 +0800

    最大公约数

--- code/test2022091601/test2022091601/test1.c
+++ code/test2022091601/test2022091601/test1.c
@@ -5,8 +5,8 @@ int main()
 	int a, b;
 	scanf("%d%d", &a, &b);
 	int c;
-	c = a >= b ? b : a;
-	/*int i;*/
+	/*c = a >= b ? b : a;*/
+	/*int i;*/              //1.
 	/*for (i = c; i >= 1;i--)
 	{
 		if ((a % i) == 0 && (b % i) == 0)
@@ -16,7 +16,7 @@ int main()
 		}
 
 	}*/
-	while (c >= 1)
+	/*while (c >= 1)              //2.
 	{
 		if ((a % c) == 0 && (b % c) == 0)
 		{
@@ -24,7 +24,30 @@ int main()
 			break;
 		}
 		c--;
-	}
+	}*/
+	//while (1)            //3.辗转相除
+	//{
+	//	if ((a % b )== 0)
+	//	{
+	//		printf("最大公约数为%d\n", b);
+	//		break;
+	//	}
+	//	else
+	//	{
+	//		c = a % b;
+	//		a = b;
+	//		b = c;
+	//	}
+	//}
+	  
 
+
+	while (a % b)    //3.1辗转相除优化
+	{
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	printf("最大公约数为%d\n", b);
 	return 0;
 }
