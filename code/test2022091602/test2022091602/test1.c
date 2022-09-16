fa780f846a42d10d9b212f35eb7e131f38c78e01
ikou <1430435375@qq.com>
2022-09-16 10:18:39 +0800

    素数判断优化

--- code/test2022091602/test2022091602/test1.c
+++ code/test2022091602/test2022091602/test1.c
@@ -1,31 +1,48 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-void prime(int k)   //素数判断         （还有一种方法为用2~k-1的数试除k）
+//void prime(int k)   //素数判断         （还有一种方法为用2~k-1的数试除k）
+//{
+//	int  i;
+//	int count = 0;
+//	for (i = (k-1); i >= 1; i--)
+//	{
+//		if ((k % i) == 0)
+//		{
+//			break;
+//		}
+//		else
+//		{
+//			count++;
+//		}
+//	}
+//	if (count == k - 2)
+//	{
+//		printf("%d是素数\n", k);
+//	}
+//}
+//int main()
+//{
+//	int i;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		prime(i);
+//	}
+//	return 0;
+//}
+void primepro(int k)     //素数判断器优化：若k为合数 则2~sqrt（k）中一定有k的因数
 {
-	int  i;
-	int count = 0;
-	for (i = (k-1); i >= 1; i--)
+	int i;
+	for (i = 2; i <= sqrt(k); i++)
 	{
-		if ((k % i) == 0)
+		if (k % i == 0)
 		{
 			break;
 		}
 		else
 		{
-			count++;
+			printf("%d是素数\n", k);
 		}
 	}
-	if (count == k - 2)
-	{
-		printf("%d是素数\n", k);
-	}
-}
-int main()
-{
-	int i;
-	for (i = 100; i <= 200; i++)
-	{
-		prime(i);
-	}
-	return 0;
+
+
 }
