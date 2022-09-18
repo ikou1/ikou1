6cf6c3428a291dcfb8fae847205894bd5d58f670
ikou <1430435375@qq.com>
2022-09-18 22:13:44 +0800

    上传22.09.18学习代码

---
+++ code/test2022091802/test2022091802/test1.c
@@ -0,0 +1,36 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int leap(int k)
+{
+	if (k % 400 == 0)
+	{
+		return 1;
+	}
+	else if (k % 100 != 0 && k % 4 == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+int main()
+{
+	int i;
+	int count = 0;
+	for (i = 1000; i <= 2000; i++)
+	{
+		if (leap(i) == 1)
+		{
+			printf("%dÄêÊÇÈòÄê\n", i);
+			count++;
+		}
+		else
+		{
+			;
+		}
+	}
+	printf("%d\n", count);
+	return 0;
+}
